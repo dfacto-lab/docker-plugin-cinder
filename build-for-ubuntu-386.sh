f0b394a5aacb5eed5771cb055aadadd2fd11ef27
Charles <479009+clecoufl@users.noreply.github.com>
2021-11-14 18:06:18 +0100

    arch fixes

--- build-for-ubuntu-386.sh
+++ build-for-ubuntu-386.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 TARGET_PLATFORM=linux
-TARGET_ARCH=386
+TARGET_ARCH=amd64
 TARGET_EXE_NAME=docker-plugin-cinder
 
 echo "Building executable ${TARGET_EXE_NAME}, for PLATFORM: ${TARGET_PLATFORM} and ARCH: ${TARGET_ARCH}"
 env GOOS=${TARGET_PLATFORM} GOARCH=${TARGET_ARCH} go build -o bin/${TARGET_EXE_NAME}
 echo "Build DONE"
+file bin/docker-plugin-cinder
